f7f5980e4575ffd9c5d7650a61fca412e9879914
Isabella da Silva Gonçalves <161075154+IsabellaSGoncalves@users.noreply.github.com>
2025-04-01 20:20:41 -0300

    ➕ build: adicionando a dependência do MongoDB

---
+++ src/database/mongodb.js
@@ -0,0 +1,32 @@
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://serenAdmins:seren123@seren.ow7lda9.mongodb.net/?retryWrites=true&w=majority&appName=Seren";
+// por favor utilizar esta conexão acima para conectar no mongodb Compass, apenas o link.
+
+// criando um cliente mongo com as configurações da api do servidor do mongo.
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+})
+
+// função apenas para testar a conexão, caso necessário dê cd em src, depois em database e escreva node mongodb.js para ver o funcionamento! 
+
+async function run() {
+  try {
+    // conectando cliente com o database.
+    await client.connect();
+    // ping para confirmar conexão.
+    await client.db("admin").command({ ping: 1 });
+    console.log("Prontinho! Você se conectou ao banco");
+  } finally {
+    // quando terminar desconecta a conexão.
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+// exportamos o módulo para que possa ser utilizado a conexão com clientes em nossos outros arquivos js.
+module.exports = { client };
