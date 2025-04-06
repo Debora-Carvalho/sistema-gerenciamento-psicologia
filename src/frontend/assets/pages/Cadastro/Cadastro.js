ea0e1dccfaff57107728fe264c76d1d9a5bbb71f
Debora-Carvalho <deboravolc2@gmail.com>
2025-04-06 20:12:45 -0300

    :sparkles: feat: criar página modelo, adicionar imagens e atualizar variáveis CSS

--- src/frontend/assets/pages/Cadastro/Cadastro.js
+++ src/frontend/assets/pages/Cadastro/Cadastro.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './Cadastro.css';
+import imgMulherCadastro from '../../images/image-mulher-cadastro.png';
 
 function Cadastro() {
     return(
         <div className='container'>
             <h1>Página de Cadastro</h1>
+            <p className='logo-seren'>Seren</p>
+            <p>Cadastre-se para gerenciar sua vida profissional de forma prática e rápida</p>
+            <img src={imgMulherCadastro} alt="Ilustração de mulher mexendo no notebook" />
         </div>
     );
 }
---
+++ src/frontend/assets/pages/PaginaModelo/PaginaModelo.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './PaginaModelo.css';
+import imgMulherLogin from '../../images/image-mulher-login.png';
+
+function PaginaModelo() {
+    return(
+        <div className='container'>
+            <h1>Esta é uma página modelo</h1>
+            <p className='logo-seren'>Seren</p>
+            <p>Verifique o css desta página para entender o funcionamento de variaveis css e estrutura React</p>
+            <img src={imgMulherLogin} alt="Ilustração de mulher mexendo no notebook" />
+        </div>
+    );
+}
+
+export default PaginaModelo;
