7e4e9d748d48469d35887ef37a12236f922d2810
Debora <deboravolc2@gmail.com>
2025-04-09 09:06:14 -0300

    :lipstick: feat: estilização do hover dos botões e links, ajustes das margens das paginas

--- src/frontend/App.js
+++ src/frontend/App.js
@@ -7,8 +7,8 @@ function App() {
     return (
         <div className="App">
             <Cadastro /> 
-            {/* <LoginEsqueciSenha /> */}
-            {/* <LoginCodigoRecuperacao /> */} 
+            {/* <LoginEsqueciSenha />  */}
+            {/* <LoginCodigoRecuperacao />  */}
         </div>
     );
 }
