b7a0888415d457a4018fd7525410d6e88c532341
BONJIU <santos.bongiovani@gmail.com>
2025-05-07 00:48:16 -0300

    🐛 fix:issues 1 e 2 implementadas

--- src/frontend/hooks/useLogin.js
+++ src/frontend/hooks/useLogin.js
@@ -1,35 +1,38 @@
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 
 export default function useLogin() {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const realizarLogin = async ({ email, senha, lembreDeMim }, setMensagemErro) => {
-        try {
-            const response = await fetch("http://localhost:4000/authLogin", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify({ email, senha }),
-            });
+  const realizarLogin = async (
+    { email, senha, lembreDeMim },
+    setMensagemErro,
+    setMensagemSucesso
+  ) => {
+    try {
+      const response = await fetch("http://localhost:4000/authLogin", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email, senha }),
+      });
 
-            const data = await response.json();
+      const data = await response.json();
 
-            if (response.ok && data.success) {
-                if (lembreDeMim) {
-                    localStorage.setItem('emailSalvo', email);
-                }
-                localStorage.setItem("userID", data.userID);
-                setMensagemErro("Login concluído! Acessando sistema...");
-                setTimeout(() => navigate("/pagina-inicial"), 1500);
-            } else {
-                setMensagemErro(data.message || "Erro ao tentar autenticar!");
-            }
-        } catch (error) {
-            console.error("Erro na requisição:", error);
-            setMensagemErro("Erro na comunicação com o servidor.");
+      if (response.ok && data.success) {
+        if (lembreDeMim) {
+          localStorage.setItem("emailSalvo", email);
         }
-    };
-
-    return { realizarLogin };
+        localStorage.setItem("userID", data.userID);
+        setMensagemSucesso("Login realizado com sucesso!");
+        setTimeout(() => navigate("/pagina-inicial"), 1500);
+      } else {
+        setMensagemErro(data.message || "Erro ao tentar autenticar!");
+      }
+    } catch (error) {
+      console.error("Erro na requisição:", error);
+      setMensagemErro("Erro na comunicação com o servidor.");
+    }
+  };
+  return { realizarLogin };
 }
