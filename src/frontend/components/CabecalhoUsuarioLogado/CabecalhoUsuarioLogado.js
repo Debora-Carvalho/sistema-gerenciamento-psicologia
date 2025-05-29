a604254c379ed16e5441631f1e610c31422faa06
AnahiMamani <narielanahi@gmail.com>
2025-05-28 21:14:19 -0300

    :bug: fix: menus responsivos totalmente corrigidos menuResponsivo, CabeçalhoReponsivo, CabeçalhoUsuarioLogado, menuPrincipal ok

--- src/frontend/components/CabecalhoUsuarioLogado/CabecalhoUsuarioLogado.js
+++ src/frontend/components/CabecalhoUsuarioLogado/CabecalhoUsuarioLogado.js
@@ -4,39 +4,32 @@ import { RiSearchLine } from "react-icons/ri";
 import useUsuarios from '../../hooks/useUsuarios';
 import FotoPerfil from '../../features/PaginaPerfil/FotoPerfil'
 
-function CabecalhoUsuarioLogado({ nomePacienteBusca, setNomePacienteBusca }) {
+function CabecalhoUsuarioLogado({ nomePacienteBusca, setNomePacienteBusca, exibirPesquisa }) {
     const { usuario } = useUsuarios();
-    const [fotoPerfilUrl, setFotoPerfilUrl] = useState(null);
-
-    // useEffect(() => {
-    //     if (usuario && usuario._id) {
-    //         setFotoPerfilUrl(`/foto/${usuario._id}`);
-    //     }
-    // }, [usuario]);
-
     if (!usuario) {
         return <div>Carregando usuário...</div>;
     }
 
     return (
-        <div className='container-cabecalho-usuario-logado'>
-            <div className="container-barra-pesquisa-usuario-logado">
-                <RiSearchLine className="icon-lupa" alt="Ícone de lupa" />
-                <input
-                    id="pesquisa-input"
-                    maxLength="800"
-                    autoCorrect="off"
-                    autoCapitalize="off"
-                    spellCheck="false"
-                    placeholder="Pesquisar paciente"
-                    value={nomePacienteBusca}
-                    onChange={(e) => setNomePacienteBusca(e.target.value)}
-                />
-            </div>
-
+        <div className={`container-cabecalho-usuario-logado ${!exibirPesquisa ? 'sem-pesquisa' : ''}`}>
+            {exibirPesquisa && (
+                <div className="container-barra-pesquisa-usuario-logado">
+                    <RiSearchLine className="icon-lupa" alt="Ícone de lupa" />
+                    <input
+                        id="pesquisa-input"
+                        maxLength="800"
+                        autoCorrect="off"
+                        autoCapitalize="off"
+                        spellCheck="false"
+                        placeholder="Pesquisar paciente"
+                        value={nomePacienteBusca}
+                        onChange={(e) => setNomePacienteBusca(e.target.value)}
+                    />
+                </div>
+            )}
             <div className='container-perfil'>
                 <div className='foto-perfil'>
-                     <FotoPerfil userId={usuario._id} />
+                    <FotoPerfil userId={usuario._id} />
                 </div>
                 <div className='infos-usuario'>
                     <p className="nome-usuario">{usuario.username?.split(' ')[0]}</p>
